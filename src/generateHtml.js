d7109c1bdd250af12cf42bb451f677d98fcdade3
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-05-09 17:52:53 -0400

    added body

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -58,3 +58,36 @@ const generateTeam = team => {
 `;
     };
 };
+
+
+module.exports = team =>  {
+
+    return`
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/ff155f92de.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="h-100 d-inline-block jumbotron mb-3 team">
+                <h1 class="text-center text-white">Meet the Team</h1>
+            </div>
+        </div>
+    </div>
+    </body>
+    </html>
+    `;
+};
