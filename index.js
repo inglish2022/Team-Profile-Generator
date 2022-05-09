750fdc6fc743581575155aefbc879277c86dea0d
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-05-09 18:28:28 -0400

    more js

--- index.js
+++ index.js
@@ -236,15 +236,15 @@ const addEngineer = () => {
 
 const writeFile = () => {
 
-    console.log('Emplyee Array: ', employeeArray)
-    // fs.writeFile('./src/generateHtml.js', data, err => {
-    //     if (err) {
-    //         console.log(err);
-    //         return;
-    //     } else {
-    //         console.log('Your team profile has been created! ')
-    //     }
-    // });
+    
+    fs.writeFile('./src/generateHtml.js', "", err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log('Your team profile has been created! ')
+        }
+    });
 };
 
 }
